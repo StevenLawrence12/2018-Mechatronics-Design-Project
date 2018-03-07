917e4eb244193e18b45afe606100c669b1a41c78
StevenLawrence12 <stevenlawrencermi@gmail.com>
2018-03-07 17:23:54 -0500

    Feat: Add turning capabilities
    Add comments

--- Motor_Control_Board/Motor_Control_Board.ino
+++ Motor_Control_Board/Motor_Control_Board.ino
@@ -2,14 +2,30 @@
  *Motor Control V1.0
  *
  */
-
+//include librarys
 #include<Servo.h>
 
+//init servo objects
 Servo servo_LeftMotor;
 Servo servo_RightMotor;
 
+//init pins
 const int LeftMotorPin=8;
 const int RightMotorPin=9;
+
+//init variables
+unsigned int leftMotorSpeed;
+unsigned int rightMotorSpeed;
+
+//drive mode; 0=stop, 1=straight, 2=left turn, 3=right turn
+unsigned int driveMode=0;
+
+//drive function
+void drive(int leftSpeed,int rightSpeed)
+{
+  servo_LeftMotor.writeMicroseconds(leftSpeed);
+  servo_RightMotor.writeMicroseconds(rightSpeed);
+}
 
 void setup() 
 {
@@ -22,7 +38,32 @@ void setup()
 
 void loop() 
 {
- servo_LeftMotor.writeMicroseconds(1600);
- Serial.println("drive");
- servo_RightMotor.writeMicroseconds(1600);
+  switch(driveMode)
+  {
+    case 0:
+    {
+      leftMotorSpeed=1500;
+      rightMotorSpeed=1500;
+      Serial.println("Stopped");
+    }
+    case 1: 
+    {
+      leftMotorSpeed=1600;
+      rightMotorSpeed=1600;
+      Serial.println("Driving Straight");
+    }
+     case 2: 
+    {
+      leftMotorSpeed=1500;
+      rightMotorSpeed=1600;
+      Serial.println("Turning Left");
+    }
+     case 3: 
+    {
+      leftMotorSpeed=1500;
+      rightMotorSpeed=1600;
+      Serial.println("Turning Right");
+    }
+  }
+ drive(leftMotorSpeed, rightMotorSpeed);
 }
