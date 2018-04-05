6e4b6d23e1424f8e4235c40fa498c30d114cec79
petarilic49 <37299986+petarilic49@users.noreply.github.com>
2018-04-04 20:04:35 -0400

    Add files via upload

---
+++ Pyramid_Garbage_Test.ino
@@ -0,0 +1,46 @@
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+int checkGarbage(int reading, int readingReference, int countNum)
+{
+  // Check to see if the ultrasonic reading is fluctuating 
+  if (reading > (readingReferance + 10) || reading < (readingReference - 10))
+  {
+    countNum++;
+  }
+
+  return countNum;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Initialize ultrasonic distance readings 
+  int ultrasonicDReading; 
+  int prevUltrasonicReading;
+  // Set the reading of the ultrasonic to prev that we will check 2 seconds later 
+
+  prevUltrasonicReading = ultrasonicDReading;
+  delay(2);
+
+  // Initalize count variable (if count exceeds 3 then pyramid is there and proceed with finding it)
+  int count = 0;
+  Serial.println("UltrasonicDReading is: ")
+  Serial.println(ultrasonicDReading);
+  Serial.println("Previous UltrasonicDReading is: ")
+  Serial.println(prevUltrasonicReading);
+  count = checkGarbage(ultrasonicDReading, prevUltrasonicReading, count);
+  Serial.println("Count is: ")
+  Serial.println(count);
+
+  if (count > 3)
+  {
+    // Reset the count for future cases
+    Serial.println("Garbage found!")
+    count = 0;
+    // Call pyramid finding function
+  }
+}
